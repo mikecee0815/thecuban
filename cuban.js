bdc25981182b34fd24cb6bbd331fb4b56f26191f
mike <mikecee0815@gmail.com>
2018-03-07 23:59:44 -0500

    first commit

---
+++ cuban.js
@@ -0,0 +1,40 @@
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('sw/sw.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw/sw.js', {scope: '/sw/'})
+//   .then(function(reg) {
+//     // registration worked
+//     console.log('Registration succeeded. Scope is ' + reg.scope);
+//   }).catch(function(error) {
+//     // registration failed
+//     console.log('Registration failed with ' + error);
+//   });
+// }
---
+++ sw/sw.js
@@ -0,0 +1,48 @@
+self.addEventListener("install", function(event) {
+  console.log('WORKER: install event in progress.');
+  event.waitUntil(
+    /* The caches built-in is a promise-based API that helps you cache responses,
+       as well as finding and deleting them.
+    */
+    caches
+      /* You can open a cache by name, and this method returns a promise. We use
+         a versioned cache name here so that we can remove old cache entries in
+         one fell swoop later, when phasing out an older service worker.
+      */
+      .open(version + 'fundamentals')
+      .then(function(cache) {
+        /* After the cache is opened, we can fill it with the offline fundamentals.
+           The method below will add all resources we've indicated to the cache,
+           after making HTTP requests for each of them.
+        */
+        return cache.addAll([
+          '/',
+          'styles.css',
+          'cuban.js'
+
+        ]);
+      })
+      .then(function() {
+        console.log('WORKER: install completed');
+      })
+  );
+});
+
+// console.log("hello from service worker");
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open('v1').then(function(cache) {
+//       return cache.addAll([
+//         'styles.css',
+//         'cuban.js'
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//   );
+// });
+
